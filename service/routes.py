df8cb04bceb3c52073830c496662d6bf030f2ea2
y44k0v <17897299+y44k0v@users.noreply.github.com>
2025-02-18 10:21:04 -0500

    read account by Id and tests

--- service/routes.py
+++ service/routes.py
@@ -67,8 +67,14 @@ def create_accounts():
 ######################################################################
 # READ AN ACCOUNT
 ######################################################################
-
-# ... place you code here to READ an account ...
+@app.route("/accounts/<int:account_id>", methods=["GET"])
+def read_account(account_id):
+    """Reads single account information by Id"""
+    app.logger.info("Request to read an Account with id: %s", account_id)
+    account = Account.find(account_id)
+    if not account:
+            abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
+    return account.serialize(), status.HTTP_200_OK
 
 
 ######################################################################
